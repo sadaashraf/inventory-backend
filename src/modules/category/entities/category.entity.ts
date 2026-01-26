d7b94e62e2045b7f55c23fa12dbb95c9ed07f718
Muhammad Ashraf <nayeemashraf92@gmail.com>
2026-01-26 13:25:12 +0500

    issue in product

--- src/modules/category/entities/category.entity.ts
+++ src/modules/category/entities/category.entity.ts
@@ -1,1 +1,16 @@
-export class Category {}
+import { Product } from "src/modules/products/entities/product.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+@Entity('categories')
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  description?: string;
+
+  @OneToMany(() => Product, product => product.category)
+  products: Product[];
+}
--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,12 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Category } from '../category/entities/category.entity';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+  constructor(
+    @InjectRepository(Product)
+    private ProductRepo: Repository<Product>,
+    @InjectRepository(Category)
+    private categoryRepe: Repository<Category>
+
+  ) { }
+
+  async create(createProductDto: CreateProductDto) {
+    const { category_id } = createProductDto;
+
+    // let category;
+
+    // if (categoryId) {
+    const category = await this.categoryRepe.findOneBy({ id: category_id });
+    //   if (!category) {
+    //     throw new NotFoundException('Category not found');
+    //   }
+    // }
+    console.log(category, "category id")
+    const product = this.ProductRepo.create({
+      ...createProductDto,
+      category, // assign the entity, not just id
+    });
+
+    return this.ProductRepo.save(product);
   }
+
 
   findAll() {
     return `This action returns all products`;
